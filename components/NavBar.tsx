f732f51921304e27b8c07a29c4a95c704ce8223b
Invincible Inventor <invincibleinventor@gmail.com>
2025-07-11 00:31:15 +0530

    ok

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -6,7 +6,7 @@ export default function NavBar(){
     return(
         <header className="text-neutral-300 bg-purple-600/10 shadow-xl">
   <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row md:items-center">
-    <nav className={`lg:space-x-5 font-medium flex flex-col mt-6 md:mt-0 space-y-2 lg:space-y-0 ${opened?'':'md:block hidden'} md:flex-row lg:w-2/5 my-2 md:my-0 md:items-center  text-base md:ml-auto`}>
+    <nav className={`lg:space-x-5 px-5 md:px-0 font-medium flex flex-col mt-6 md:mt-0 space-y-2 lg:space-y-0 ${opened?'':'md:block hidden'} md:flex-row lg:w-2/5 my-2 md:my-0 md:items-center  text-base md:ml-auto`}>
       <a className=" cursor-pointer hover:text-neutral-200">Why OneRoof?</a>
       <a className=" cursor-pointer hover:text-neutral-200">The Team</a>
       <a className=" cursor-pointer hover:text-neutral-200">Support</a>
@@ -19,7 +19,7 @@ export default function NavBar(){
     <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} d="M20 12H10m10-7H4m16 14H4" color="currentColor"></path></svg>
     </button>
     </a>
-    <div className={`lg:w-2/5  inline-flex lg:justify-end md:ml-5 lg:ml-0 ${opened?'':'md:inline-flex hidden'}`}>
+    <div className={`lg:w-2/5  inline-flex lg:justify-end ml-5 lg:ml-0 ${opened?'':'md:inline-flex hidden'}`}>
       <button className="inline-flex transition-all ease-linear duration-300 items-center text-neutral-300 font-semibold bg-white/10 border-0 py-2 px-5 focus:outline-none hover:bg-white/20 rounded-lg  text-base mt-4 md:mt-0">Sign Up
         <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
           <path d="M5 12h14M12 5l7 7-7 7"></path>
