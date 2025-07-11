4bdfb0f0bff3fae404f7119134d5009899a75b10
Invincible Inventor <invincibleinventor@gmail.com>
2025-07-11 16:41:36 +0530

    ok

--- app/page.tsx
+++ app/page.tsx
@@ -2,39 +2,42 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-   <div className="bg-gradient-to-t from-neutral-950 to-purple-800/10">
+   <div className="bg-neutral-950 ">
     <section className="">
   <div className="py-8 px-10 mx-auto max-w-screen-xl w-full flex flex-col items-center content-center sm:py-14 lg:px-6">
       <div className="max-w-screen-md w-full mb-10 md:mt-0 mt-4 md:mb-14">
           <h2 className="mb-4 text-4xl tracking-tight text-center mx-auto font-semibold text-white">One central place for all your needs</h2>
           <p className=" sm:text-base my-5 text-gray-300 text-center mx-auto">Here at OneRoof, we provide the one central solution for all the needs of every SASTRA student. From a rental marketplace - to a lost and find system, we have got everything covered for you!</p>
           <div className="flex mx-auto w-max md:flex-row md:items-end content-end md:space-x-2 flex-col space-x-0 space-y-2 md:space-y-0 mt-8">
-              <button className="font-poppins rounded-full text-sm bg-purple-500/40  px-8 py-4 hover:scale-105 duration-200 transition-all ease-linear hover:bg-neutral-600/10 text-white font-semibold">Get Started</button>
+              <button className="button-primary">Get Started</button>
                 <button className="font-poppins rounded-xl text-sm  px-8 py-4 hover:scale-105 duration-200 transition-all ease-linear  text-white font-semibold">Know More</button>
           </div>
           
       </div>
+     
       <div className="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0">
           
           
          
-          <div className="shadow-xl hover:scale-105 duration-200 ease-linear transition-all bg-purple-900 bg-opacity-5 rounded-3xl px-6 py-12">
-              <div className="flex justify-center items-center mx-auto mb-4 rounded-2xl w-max p-6 bg-gradient-to-t from-white/10 to-purple-500/10">
+          <div className="shadow-xl duration-200 ease-linear transition-all bg-black bg-opacity-0  px-6 py-12">
+              <div className="flex justify-center items-center mx-auto mb-4 w-max p-6 bg-neutral-800/30">
                  <svg width="24" height="24" className="w-8 h-8  text-white" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path fill="none" stroke="currentColor" d="M9.5 11v-1a2.5 2.5 0 0 1 5 0v.039a2 2 0 0 1-.75 1.562l-1 .798a2 2 0 0 0-.75 1.562V15.5m0 1.5v2M5.5 4.5v17m13 0v-17h-2.027m-8.946 0H.5v.25l.055.31a45.7 45.7 0 0 1 0 15.88l-.055.31v.25h23v-.25l-.055-.31a45.7 45.7 0 0 1 0-15.88l.055-.31V4.5h-7.027m-8.946 0a4.5 4.5 0 0 1 8.946 0m-8.946 0h8.946"/></svg>
               </div>
               <h3 className="mb-2 text-lg font-bold mx-auto text-center text-white">Lost Items</h3>
               <p className="text-gray-400 text-sm lg:text-base mx-auto px-6 text-center">Post items that you have lost on the public billboard to alert others and possibly aid you in finding them back.</p>
           </div>
-         <div className="shadow-xl hover:scale-105 duration-200 ease-linear transition-all bg-pink-900 bg-opacity-5 rounded-3xl px-6 py-12">
-              <div className="flex justify-center mx-auto items-center mb-4 rounded-2xl w-max p-6 bg-gradient-to-t from-white/10 to-pink-500/10">
-               <svg width="24" height="24" className="text-white w-8 h-8" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g fill="none" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M3 21h2m16 0h-2M5 21h14M5 21V9.328M19 21V9.328m-14 0a2 2 0 0 1-.11-.068l-.54-.36a2 2 0 0 1-.747-2.407l.894-2.236A2 2 0 0 1 6.354 3h11.292a2 2 0 0 1 1.857 1.257l.894 2.236A2 2 0 0 1 19.65 8.9l-.54.36a2 2 0 0 1-.11.068m-14 0a2 2 0 0 0 2.11-.068L9 8l1.89 1.26a2 2 0 0 0 2.22 0L15 8l1.89 1.26a2 2 0 0 0 2.11.068"/><path d="M14 21v-5a2 2 0 1 0-4 0v5"/></g></svg>
+     <div className="shadow-xl duration-200 ease-linear transition-all bg-black bg-opacity-0  px-6 py-12">
+                        
+
+              <div className="flex justify-center mx-auto items-center mb-4  w-max p-6 bg-neutral-800/30">
+               <svg width="24" height="24" className="text-white w-8 h-8" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g fill="none" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" d="M3 21h2m16 0h-2M5 21h14M5 21V9.328M19 21V9.328m-14 0a2 2 0 0 1-.11-.068l-.54-.36a2 2 0 0 1-.747-2.407l.894-2.236A2 2 0 0 1 6.354 3h11.292a2 2 0 0 1 1.857 1.257l.894 2.236A2 2 0 0 1 19.65 8.9l-.54.36a2 2 0 0 1-.11.068m-14 0a2 2 0 0 0 2.11-.068L9 8l1.89 1.26a2 2 0 0 0 2.22 0L15 8l1.89 1.26a2 2 0 0 0 2.11.068"/><path d="M14 21v-5a2 2 0 1 0-4 0v5"/></g></svg>
               </div>
               <h3 className="mb-2 text-lg font-bold mx-auto text-center text-white">Marketplace</h3>
               <p className="text-sm lg:text-base text-gray-400 mx-auto text-center px-6">Sell your old stuff, excess items or previous year books, unused notebooks, lab manuals, class notes etc on the marketplace</p>
           </div>
-          <div className="shadow-xl hover:scale-105 duration-200 ease-linear transition-all bg-green-900 bg-opacity-5 rounded-3xl px-6 py-12">
-              <div className="flex mx-auto justify-center items-center mb-4 rounded-2xl w-max p-6 bg-gradient-to-t from-white/10 to-green-500/10">
-               <svg width="512" height="512" className="w-8 h-8 text-white" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><path fill="currentColor" fill-rule="evenodd" d="m256 34.347l192 110.851v221.703L256 477.752L64 366.901V145.198zM106.666 192.001v150.266l128 73.9V265.902zm298.667.001l-128 73.9v150.265l128-73.9zM256 83.614l-125.867 72.67L256 228.952l125.867-72.67z"/></svg>
+      <div className="shadow-xl duration-200 ease-linear transition-all bg-black bg-opacity-0   px-6 py-12">
+              <div className="flex mx-auto justify-center items-center mb-4  w-max p-6 bg-neutral-800/30">
+               <svg width="512" height="512" className="w-8 h-8 text-white" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><path fill="currentColor" fillRule="evenodd" d="m256 34.347l192 110.851v221.703L256 477.752L64 366.901V145.198zM106.666 192.001v150.266l128 73.9V265.902zm298.667.001l-128 73.9v150.265l128-73.9zM256 83.614l-125.867 72.67L256 228.952l125.867-72.67z"/></svg>
               </div>
               <h3 className="mb-2 text-lg font-bold mx-auto text-center text-white">Project Showcase</h3>
               <p className="text-gray-400 px-6 lg:text-base text-sm mx-auto text-center">Showcase your projects - both hobby or professional ones on the public showcase for others to view, support and critique</p>
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -4,23 +4,23 @@ import { useState } from "react"
 export default function NavBar(){
     const [opened,setOpened] =useState(false)
     return(
-        <header className="text-neutral-300 bg-purple-600/10 shadow-xl">
+        <header className="text-neutral-300  bg-purple-200 border-b border-b-neutral-900 shadow-xl">
   <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row md:items-center">
-    <nav className={`lg:space-x-5 px-5 md:px-0 font-medium flex flex-col mt-6 md:mt-0 space-y-2 lg:space-y-0 ${opened?'':'md:block hidden'} md:flex-row lg:w-2/5 my-2 md:my-0 md:items-center  text-base md:ml-auto`}>
-      <a className=" cursor-pointer hover:text-neutral-200">Why OneRoof?</a>
-      <a className=" cursor-pointer hover:text-neutral-200">The Team</a>
-      <a className=" cursor-pointer hover:text-neutral-200">Support</a>
-      <a className="hover:text-neutral-200 cursor-pointer">GitHub</a>
+    <nav className={`lg:space-x-5 px-5 md:px-0 font-pops  text-black font-semibold flex flex-col mt-6 md:mt-0 space-y-2 lg:space-y-0 ${opened?'':'md:block hidden'} md:flex-row lg:w-2/5 my-2 md:my-0 md:items-center  text-base md:ml-auto`}>
+      <a className=" cursor-pointer hover:text-neutral-900">Why OneRoof?</a>
+      <a className=" cursor-pointer hover:text-neutral-900">The Team</a>
+      <a className=" cursor-pointer hover:text-neutral-900">Support</a>
+      <a className="hover:text-neutral-900 cursor-pointer">GitHub</a>
     </nav>
-    <a className="flex order-first lg:order-none lg:w-1/5 title-font font-medium items-center text-white lg:items-center lg:justify-center mb-0 md:mb-0">
-    <svg xmlns="http://www.w3.org/2000/svg" className="hidden bg-purple-500/30 text-lg border-white rounded-full text-white w-8 h-8 p-[4px]" stroke-width="10" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M11 19q-.671 0-1.143-.472t-.472-1.144v-2q0-.67.472-1.143q.472-.472 1.143-.472h2q.671 0 1.143.472t.472 1.143v2q0 .672-.472 1.144T13 19zm-8-7.061q-.177.115-.365.094q-.189-.021-.323-.198q-.135-.177-.111-.366t.195-.323l8.629-6.433q.217-.161.472-.232T12 4.409t.503.072t.472.232l3.371 2.51v-1.28q0-.395.274-.669T17.288 5t.669.274t.274.668V8.62l3.398 2.527q.171.135.192.323q.021.189-.113.365q-.135.177-.323.199q-.189.02-.366-.095l-8.653-6.413Q12.212 5.41 12 5.41t-.365.115z"></path></svg>
+    <a className="flex order-first lg:order-none lg:w-1/5 title-font font-medium items-center text-black lg:items-center lg:justify-center mb-0 md:mb-0">
+    <svg xmlns="http://www.w3.org/2000/svg" className="hidden bg-purple-500/30 text-lg border-black rounded-full text-white w-8 h-8 p-[4px]" stroke-width="10" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M11 19q-.671 0-1.143-.472t-.472-1.144v-2q0-.67.472-1.143q.472-.472 1.143-.472h2q.671 0 1.143.472t.472 1.143v2q0 .672-.472 1.144T13 19zm-8-7.061q-.177.115-.365.094q-.189-.021-.323-.198q-.135-.177-.111-.366t.195-.323l8.629-6.433q.217-.161.472-.232T12 4.409t.503.072t.472.232l3.371 2.51v-1.28q0-.395.274-.669T17.288 5t.669.274t.274.668V8.62l3.398 2.527q.171.135.192.323q.021.189-.113.365q-.135.177-.323.199q-.189.02-.366-.095l-8.653-6.413Q12.212 5.41 12 5.41t-.365.115z"></path></svg>
       <span className="ml-3 text-xl font-bold">TheOneRoof</span>
       <button onClick={()=>setOpened(!opened)} className={`md:hidden transition-all ease-linear duration-300 block w-max w-8 h-8 ml-auto rounded-lg  p-2 bg-neutral-800 hover:bg-neutral-700`}>
     <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} d="M20 12H10m10-7H4m16 14H4" color="currentColor"></path></svg>
     </button>
     </a>
     <div className={`lg:w-2/5  inline-flex lg:justify-end ml-5 lg:ml-0 ${opened?'':'md:inline-flex hidden'}`}>
-      <button className="inline-flex transition-all ease-linear duration-300 items-center text-neutral-300 font-semibold bg-white/10 border-0 py-2 px-5 focus:outline-none hover:bg-white/20 rounded-lg  text-base mt-4 md:mt-0">Sign Up
+      <button className="inline-flex rounded-full transition-all ease-linear duration-300 items-center text-white font-semibold bg-black border-0 py-2 px-5 focus:outline-none hover:bg-black/90 text-base mt-4 md:mt-0">Sign Up
         <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
           <path d="M5 12h14M12 5l7 7-7 7"></path>
         </svg>
